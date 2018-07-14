1084c17095a585efb080def2f879d0ba6f5c892a
Zed <dev@zedcn.com>
2018-07-14 09:36:12 +0800

    更新脚本

--- restart_plug.sh
+++
@@ -1,4 +0,0 @@
-git pull --rebase
-cp -f ./helper.py ~/.qqbot-tmp/plugins/
-qq unplug helper
-qq plug helper
---
+++ update_plugin.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git pull --rebase
+if [[ $? -eq 0 ]];then
+    cp -f ./helper.py ~/.qqbot-tmp/plugins/
+    qq unplug helper
+    qq plug helper
+fi
