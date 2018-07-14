2ecff72c1edc092d8fb0ad1eeee6a78513b67c40
Zed <dev@zedcn.com>
2018-07-14 14:08:00 +0800

    完全通过helpers注册指令

--- abotcmd.py
+++ abotcmd.py
@@ -9,23 +9,26 @@ def toolProcess(context):
     if cmd == None:
         send(context,'解析错误，请以【@QQBot tool cmd】或者【@QQBot 工具 命令】格式发送。发送【@QQBot tool help】查询所有命令')
         return
-    elif any(k in cmd for k in ['解析域名','nslookup']):
-        cmdNslookup(context,cmd)
-    elif any(k in cmd for k in ['help','帮助']):
+    for helper in cmdHelpers:
+        if any(k in cmd for k in [helper.cmd,helper.cmdCN]):
+            helper.method(context,cmd)
+            return
+    if any(k in cmd for k in ['help','帮助']):
         cmdHelp(context)
+    else:
+        send(context,'未知命令，发送【@QQBot tool help】查询所有命令')
 
 class CmdHelper:
-    def __init__(self, cmd, cmdCN, desc, etc):
+    def __init__(self, method, cmd, cmdCN, desc, etc):
+        self.method = method
         self.cmd = cmd
         self.cmdCN = cmdCN
         self.desc = desc
         self.etc = etc
-
-cmdHelpers = [
-    CmdHelper('nslookup','解析域名','返回指定域名的DNS解析结果','直接解析：nslookup z.cn，指定dns解析：nslookup z.cn 8.8.8.8')
-]
-       
-
+     
+def cmdIsInvalid(cmd):
+    #TODO 也许有更好的处理办法
+    return any(k in cmd for k in ['&','\\','>','<'])
 
 def getToolCMD(txt):
     m = re.match(r"^\s*[tool|工具]+\s*(\S.*)$",txt)
@@ -49,11 +52,11 @@ def cmdNslookup(context,cmd):
         send(context,"包含非法字符")
         return
     if m == None or len(m[1]) <= 1: #不要问我这里为什么会等于1，鬼知道
-        send(context,'解析错误。{0}'.format(help.etc))
+        send(context,'参数错误。{0}'.format(help.etc))
     else:
-        send(context,subprocess.getoutput('nslookup ' + m[1]))
+        send(context,subprocess.getoutput(help.cmd + ' ' + m[1]))
 
-
-def cmdIsInvalid(cmd):
-    #TODO 也许有更好的处理办法
-    return any(k in cmd for k in ['&','\\','>','<'])
+# 注册工具集
+cmdHelpers = [
+    CmdHelper(cmdNslookup,'nslookup','解析域名','返回指定域名的DNS解析结果','直接解析：nslookup z.cn，指定dns解析：nslookup z.cn 8.8.8.8')
+]
